54fcb331d2ddcf2e07c548cee7cc36d0d9e8ef66
ronak-goyal-code <ronakgoyal1403@gmail.com>
2026-04-04 20:02:38 +0530

    filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -4,6 +4,7 @@ function Filter() {
   return (
     <div>
       Filter
+      Added a filter component to the project. This component will allow users to filter the list of items based on certain criteria. The filter options will be displayed as checkboxes, and users can select multiple options to narrow down their search results. The filter component will also include a reset button to clear all selected filters and show the full list of items again.
 
     </div>
   )
